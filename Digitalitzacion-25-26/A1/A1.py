b840427d6817bb26832706692cfb723e53256704
Eduardo <lobatoeduardi@gmail.com>
2025-12-01 21:24:04 +0100

    A1

---
+++ Digitalitzacion-25-26/A1/A1.py
@@ -0,0 +1,15 @@
+print("Hello World!!!")
+a=5
+b=2.1
+s="hello"
+print(type(s))
+print(type(a))
+print(type(b))
+if a == 5:
+     print("Dentro IF") 
+     print("Dentro IF 2")
+
+print("Fuera del IF") 
+
+for x in range(2,9):
+     print(x)
